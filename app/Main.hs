abe7983663e3f39dabb996fb48d7abe892261068
Nikita Gaidakov <ngaidakov@podfather.com>
2021-09-28 15:13:46 +0100

    Implement hello world

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+import qualified Network.HTTP.Types as HTTP
+import qualified Network.Wai as Wai
+import Network.Wai.Handler.Warp (run)
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn $ "running on http://localhost:" <> show port <> "/"
+  run port app
+
+app :: Wai.Application
+app _ send = send $ Wai.responseLBS HTTP.status200 [("Content-Type", "text/plain")] "Hello world!"
+
+port :: Int
+port = 8080
