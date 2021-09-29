d542c0dd1b19f0c0f4fd241132fcb420d777066b
Nikita Gaidakov <ngaidakov@podfather.com>
2021-09-29 10:27:59 +0100

    Add websockets, with echoing ws app

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,34 @@
 
 module Main where
 
+import Control.Monad (forever)
+import qualified Data.Text.IO as T
 import qualified Network.HTTP.Types as HTTP
 import qualified Network.Wai as Wai
 import Network.Wai.Handler.Warp (run)
+import Network.Wai.Handler.WebSockets (websocketsOr)
+import qualified Network.WebSockets as WS
 
 main :: IO ()
 main = do
   putStrLn $ "running on http://localhost:" <> show port <> "/"
-  run port app
+  run port $ websocketsOr WS.defaultConnectionOptions wsApp httpApp
 
-app :: Wai.Application
-app _ send = send $ Wai.responseLBS HTTP.status200 [("Content-Type", "text/plain")] "Hello world!"
+httpApp :: Wai.Application
+httpApp _ send = send $ Wai.responseLBS HTTP.status200 [("Content-Type", "text/plain")] "Hello world!"
+
+wsApp :: WS.ServerApp
+wsApp pending = do
+  con <- WS.acceptRequest pending
+  putStrLn "Accepted new connection"
+  WS.withPingThread con 30 (return ()) $ echo con
+
+echo :: WS.Connection -> IO ()
+echo con =
+  forever $ do
+    msg <- WS.receiveData con
+    T.putStrLn $ "Message received: " <> msg
+    WS.sendTextData con $ "Message received: " <> msg
 
 port :: Int
 port = 8080
