34fedd530b4aa8a8fad768a6bbe710274963eba3
Nikita Gaidakov <ngaidakov@podfather.com>
2021-09-29 16:01:09 +0100

    Add static file serving

--- app/Main.hs
+++ app/Main.hs
@@ -6,17 +6,24 @@ import Control.Monad (forever)
 import qualified Data.Text.IO as T
 import qualified Network.HTTP.Types as HTTP
 import qualified Network.Wai as Wai
-import Network.Wai.Handler.Warp (run)
+import qualified Network.Wai.Application.Static as Static
+import qualified Network.Wai.Handler.Warp as Warp
 import Network.Wai.Handler.WebSockets (websocketsOr)
 import qualified Network.WebSockets as WS
 
 main :: IO ()
 main = do
-  putStrLn $ "running on http://localhost:" <> show port <> "/"
-  run port $ websocketsOr WS.defaultConnectionOptions wsApp httpApp
+  putStrLn $ "running on http://localhost:" <> show port
+  Warp.run port $ websocketsOr WS.defaultConnectionOptions wsApp staticWithRoot
 
-httpApp :: Wai.Application
-httpApp _ send = send $ Wai.responseLBS HTTP.status200 [("Content-Type", "text/plain")] "Hello world!"
+staticWithRoot :: Wai.Application
+staticWithRoot req respond =
+  case Wai.rawPathInfo req of
+    "/" -> respond $ Wai.responseFile HTTP.status200 [("Content-Type", "text/html")] "public/index.html" Nothing
+    _ -> staticApp req respond
+
+staticApp :: Wai.Application
+staticApp = Static.staticApp $ Static.defaultWebAppSettings publicDir
 
 wsApp :: WS.ServerApp
 wsApp pending = do
@@ -33,3 +40,6 @@ echo con =
 
 port :: Int
 port = 8080
+
+publicDir :: String
+publicDir = "public/"
