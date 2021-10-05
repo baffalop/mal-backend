b165b7938c540b41a56b759da206eb7635b9d438
Nikita Gaidakov <ngaidakov@podfather.com>
2021-10-05 22:51:02 +0100

    Maintain state with MVars, hacky connection management

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,10 @@
 
 module Main where
 
-import Control.Monad (forever)
+import Control.Arrow ((&&&))
+import Control.Concurrent.MVar
+import Control.Exception (finally)
+import Control.Monad (forM_, forever)
 import qualified Data.Aeson as Aeson
 import qualified Data.ByteString.Lazy.Char8 as LB
 import qualified Network.HTTP.Types as HTTP
@@ -12,7 +15,7 @@ import qualified Network.Wai.Handler.Warp as Warp
 import Network.Wai.Handler.WebSockets (websocketsOr)
 import qualified Network.WebSockets as WS
 import qualified World
-import World (DownMsg(..), UpMsg(..))
+import World (DownMsg(..), UpMsg(..), World)
 
 port :: Int
 port = 8080
@@ -23,7 +26,9 @@ publicDir = "public/"
 main :: IO ()
 main = do
   putStrLn $ "running on http://localhost:" <> show port
-  Warp.run port $ websocketsOr WS.defaultConnectionOptions wsApp staticWithRoot
+  world <- newMVar World.new
+  clients <- newMVar ([] :: [WS.Connection])
+  Warp.run port $ websocketsOr WS.defaultConnectionOptions (wsApp clients world) staticWithRoot
 
 staticWithRoot :: Wai.Application
 staticWithRoot req respond =
@@ -34,14 +39,16 @@ staticWithRoot req respond =
 staticApp :: Wai.Application
 staticApp = Static.staticApp $ Static.defaultWebAppSettings publicDir
 
-wsApp :: WS.ServerApp
-wsApp pending = do
+wsApp :: MVar [WS.Connection] -> MVar World -> WS.ServerApp
+wsApp cons world pending = do
   con <- WS.acceptRequest pending
   putStrLn "Accepted new connection"
-  WS.withPingThread con 30 (return ()) $ world con
+  index <- modifyMVar cons $ pure . ((<> [con]) &&& length)
+  let disconnect = putStrLn "Disconnected" >> modifyMVar_ cons (pure . withoutIndex index)
+  (keepAlive con $ worldApp cons con world) `finally` disconnect
 
-world :: WS.Connection -> IO ()
-world con =
+worldApp :: MVar [WS.Connection] -> WS.Connection -> MVar World -> IO ()
+worldApp cons con world =
   forever $ do
     msg <- WS.receiveData con
     LB.putStrLn $ "Message received: " <> msg
@@ -50,4 +57,22 @@ world con =
         putStrLn "Invalid UpMsg"
       Just (Insert index newSpan) -> do
         putStrLn $ "Inserting new span at layer " <> show index
-        WS.sendTextData con $ Aeson.encode $ Update $ World.insert newSpan index World.new
+        newWorld <- modifyReturnMVar world $ World.insert newSpan index
+        broadcast cons $ Update newWorld
+
+broadcast :: MVar [WS.Connection] -> DownMsg -> IO ()
+broadcast cons msg = do
+  connections <- readMVar cons
+  forM_ connections $ flip WS.sendTextData $ Aeson.encode msg
+
+keepAlive :: WS.Connection -> IO () -> IO ()
+keepAlive con = WS.withPingThread con 30 (return ())
+
+modifyReturnMVar :: MVar a -> (a -> a) -> IO a
+modifyReturnMVar var f = modifyMVar var $ pure . dup . f
+
+dup :: a -> (a, a)
+dup x = (x, x)
+
+withoutIndex :: Int -> [a] -> [a]
+withoutIndex index list = take index list ++ drop (index + 1) list
