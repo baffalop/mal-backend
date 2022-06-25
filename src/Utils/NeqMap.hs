3daeb0e0e0a5dc3917c4271cc0125bc46d8250bc
Nikita Gaidakov <ngaidakov@podfather.com>
2022-06-25 08:38:29 +0100

    Format NeqMap module a little nicer

--- src/Utils/NeqMap.hs
+++ src/Utils/NeqMap.hs
@@ -21,8 +21,11 @@ empty = NeqMap 0 M.empty
 toList :: NeqMap a -> [a]
 toList (NeqMap _ map) = fmap snd $ M.toList map
 
-insert :: a -> NeqMap a -> (NeqMap a, Int)
-insert x (NeqMap index map) = (NeqMap (index + 1) (M.insert index x map), index)
+insert :: a -> NeqMap a -> ( NeqMap a, Int )
+insert x (NeqMap index map) =
+  ( NeqMap (index + 1) $ M.insert index x map
+  , index
+  )
 
 delete :: Int -> NeqMap a -> NeqMap a
-delete i map = map {_map = M.delete i $ _map map}
+delete i map = map { _map = M.delete i $ _map map }
